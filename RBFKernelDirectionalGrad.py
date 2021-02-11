dd03411360a0a82c784f79a8807a54b1a5072a50
Misha Padidar <map454@cornell.edu>
2021-02-10 22:09:33 -0500

    starting to make changes after 2/10 meeting with jake

--- RBFKernelDirectionalGrad.py
+++ RBFKernelDirectionalGrad.py
@@ -54,21 +54,27 @@ class RBFKernelDirectionalGrad(RBFKernel):
 
     """
 
-    def forward(self, x1, x2, diag=False, **params):
+    def forward(self, x1, x2, v1,v2,diag=False, **params):
+
+        # TODO:
+        # 1. pass in the directions for x1 and x2
+        # 
+
         batch_shape = x1.shape[:-2]
         n_batch_dims = len(batch_shape)
         n1, d = x1.shape[-2:]
         n2 = x2.shape[-2]
         # directions
-        V  = params['V']
-        n_dir = V.shape[-2] # number of directions
-        assert self.n_dir == n_dir, "V.shape[-2] != self.n_dir"
+        n_dir1 = v1.shape[-2]
+        n_dir2 = v2.shape[-2]
+
         # normalize directions
-        V = (V.T/torch.norm(V,dim=1)).T
+        v1 = (v1.T/torch.norm(v1,dim=1)).T
+        v2 = (v2.T/torch.norm(v1,dim=1)).T
 
 
         # n1*(dim+1) x n2*(dim+1)
-        K = torch.zeros(*batch_shape, n1 * (n_dir+ 1), n2 * (n_dir+ 1), device=x1.device, dtype=x1.dtype)
+        K = torch.zeros(*batch_shape, n1 * (n_dir1+ 1), n2 * (n_dir2+ 1), device=x1.device, dtype=x1.dtype)
 
         if not diag:
             # Scale the inputs by the lengthscale (for stability)
@@ -76,11 +82,18 @@ class RBFKernelDirectionalGrad(RBFKernel):
             x2_ = x2.div(self.lengthscale)
 
             # project inputs onto directions
-            x1_v = x1_ @ V.T
-            x2_v = x2_ @ V.T
+            x1_v1 = x1_ @ v1.T
+            x1_v2 = x1_ @ v2.T
+            x2_v1 = x2_ @ v1.T
+            x2_v2 = x2_ @ v2.T
 
+            # TODO:
+            # the outer for each block is different
+            # and depends on which set of directions
+            # use the appropriate xi_vj
+            #
             # Form all possible rank-1 products for the gradient and Hessian blocks
-            outer = x1_v.view(*batch_shape, n1, 1, n_dir) - x2_v.view(*batch_shape, 1, n2, n_dir)
+            outer = x1_v.view(*batch_shape, n1, 1, n_dir1) - x2_v.view(*batch_shape, 1, n2, n_dir2)
             outer = outer / self.lengthscale.unsqueeze(-2)
             outer = torch.transpose(outer, -1, -2).contiguous()
 
@@ -89,15 +102,25 @@ class RBFKernelDirectionalGrad(RBFKernel):
             K_11 = diff
             K[..., :n1, :n2] = K_11
 
+            # TODO:
+            # use directions for x2 to compute this block
+            # 
             # 2) First gradient block # (n1 x n2*n_dir)
-            outer1 = outer.view(*batch_shape, n1, n2 * n_dir)
+            outer1 = outer.view(*batch_shape, n1, n2 * n_dir1)
             K[..., :n1, n2:] = outer1 * K_11.repeat([*([1] * (n_batch_dims + 1)), n_dir])
 
+            # TODO:
+            # use directions for x1 to compute this block
+            # 
             # 3) Second gradient block
             outer2 = outer.transpose(-1, -3).reshape(*batch_shape, n2, n1 * n_dir)
             outer2 = outer2.transpose(-1, -2)
             K[..., n1:, :n2] = -outer2 * K_11.repeat([*([1] * n_batch_dims), n_dir, 1])
 
+
+            # TODO:
+            # use directions for both x1 and x2 to compute this block
+            # 
             # 4) Hessian block
             outer3 = outer1.repeat([*([1] * n_batch_dims), n_dir, 1]) * outer2.repeat([*([1] * (n_batch_dims + 1)), n_dir])
             kp = KroneckerProductLazyTensor(
@@ -111,11 +134,6 @@ class RBFKernelDirectionalGrad(RBFKernel):
             if n1 == n2 and torch.eq(x1, x2).all():
                 K = 0.5 * (K.transpose(-1, -2) + K)
 
-            # Apply a perfect shuffle permutation to match the MutiTask ordering
-            pi1 = torch.arange(n1 * (n_dir + 1)).view(n_dir + 1, n1).t().reshape((n1 * (n_dir + 1)))
-            pi2 = torch.arange(n2 * (n_dir + 1)).view(n_dir + 1, n2).t().reshape((n2 * (n_dir + 1)))
-            K = K[..., pi1, :][..., :, pi2]
-            
             return K
 
         else:
@@ -126,14 +144,14 @@ class RBFKernelDirectionalGrad(RBFKernel):
             grad_diag = torch.ones(*batch_shape, n2, n_dir, device=x1.device, dtype=x1.dtype) / self.lengthscale.pow(2)
             grad_diag = grad_diag.transpose(-1, -2).reshape(*batch_shape, n2 * n_dir)
             k_diag = torch.cat((kernel_diag, grad_diag), dim=-1)
-            pi = torch.arange(n2 * (n_dir + 1)).view(n_dir + 1, n2).t().reshape((n2 * (n_dir + 1)))
-            return k_diag[..., pi]
+            return k_diag
 
+
+    # TODO:
+    # what is the number of outputs per input
     def num_outputs_per_input(self, x1, x2):
-        return self.n_dir + 1
+        return #self.n_dir + 1
 
-    def set_n_dir(self,n_dir):
-      self.n_dir = n_dir
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -3,46 +3,76 @@ import numpy as np
 import torch
 import gpytorch
 import tqdm
+import random
 from matplotlib import pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
 from RBFKernelDirectionalGrad import RBFKernelDirectionalGrad
+from DirectionalGradVariationalStrategy import DirectionalGradVariationalStrategy
 
+"""Notes from Jake
+- we will pass in mini-batches of data stochastically with a fixed size of observations,
+  i.e. function values plus fixed number of derivatives
+- when passing in training data we should pass in X,[fX,df/dW],W where W is the set
+  of canonical directions that the mini-batch of derivatives df/dW are taken in.
+- upon taking block of observations we should reshape observations as a giant vector
+  so that [fX,df/dW] is a giant vector. This means we should repeat the X values so that 
+  the vector of observations is the same number of entries as X. 
+- The kernel K_{XZ} is computes the kernel at the points X and Z in the directions U and V. 
+  When X is training points the directions are W, the canonical directions that match up
+  with the derivative directions of the mini-batch. The directions V are the inducing directions. 
+  We must rewrite the kernel to require two sets of points and two sets of directions.
+- q(f) should be a vector of same size as the number of observations. We should edit the
+  base variational strategy so that we can pass in kernel directions. I beleive that we can
+  do this through kwargs.
+- make sure to only return the diagonal of q(f)
+- One big takeaway is that their is no multitasking at all!"""
 
+"""Future Upgrades
+- don't include function values in every training iteration... be truly stochastic.
+"""
 
-class MultitaskGPModel(gpytorch.models.ApproximateGP):
+class GPModel(gpytorch.models.ApproximateGP):
     def __init__(self,num_inducing,num_directions,dim):
 
         self.num_directions = num_directions
-        self.num_inducing = num_inducing
+        self.num_inducing   = num_inducing
         # Let's use a different set of inducing points for each latent function
         inducing_points     = torch.rand(num_inducing, dim)
         inducing_directions = torch.eye(dim)[:num_directions] # canonical directions
         num_directional_derivs = num_directions*num_inducing
 
         # variational distribution q(u,g)
-        # use batch so we learn a variational distribution for each task
         variational_distribution = gpytorch.variational.DeltaVariationalDistribution(
-            num_inducing+num_directional_derivs,batch_shape=torch.Size([inducing_directions+1]))
+            num_inducing + num_directional_derivs)
         # variational strategy q(f)
-        # TODO: 
-        # - wrap with multioutput (see example)
-        variational_strategy = gpytorch.variational.VariationalStrategy(
-                self, inducing_points, variational_distribution, learn_inducing_locations=True)
+        variational_strategy = DirectionalGradVariationalStrategy(gpytorch.variational.VariationalStrategy(
+                self, inducing_points, variational_distribution, learn_inducing_locations=True),
+        num_directions
+        )
         super().__init__(variational_strategy)
 
         # TODO: 
         # - mark the mean and covariance as batch (see example)
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(RBFKernelDirectionalGrad(num_directions))
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([num_directions+1]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(RBFKernelDirectionalGrad(batch_shape=torch.Size([num_directions+1])),
+          batch_shape=torch.Size([num_directions+1]))
         # set the number of directions
         self.covar_module.base_kernel.set_n_dir(num_directions)
 
+        # this is the LMC covar... just here for testing
+        # self.covar_module = gpytorch.kernels.ScaleKernel(
+        #     gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_directions+1])),
+        #     batch_shape=torch.Size([num_directions+1])
+        # )
+
         # register the directions
         self.register_parameter(name="inducing_directions", parameter=torch.nn.Parameter(inducing_directions))
+        # print(variational_strategy(torch.rand(10, dim)).shape)
+        # quit()
 
 
 
-    def forward(self, x):
+    def forward(self, x, derivative_directions):
 
         # pass in params
         params = {}
@@ -54,32 +84,42 @@ class MultitaskGPModel(gpytorch.models.ApproximateGP):
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
-def sample_contiguous_columns(n_max,n_cols,n_min=0):
+def select_cols_of_y(y_batch,minibatch_dim,dim):
   """
-  n_max: maximum index;
-  n_cols: number of columns to select
-  n_min: minimum index;
-  return: list of indexes of contiguous selected columns
+  randomly select columns of y to train on, but always select 
+  function values as part of the batch. Otherwise we have
+  to keep track of whether we passed in function values or not
+  when computing the kernel.
+
+  input
+  y_batch: 2D-torch tensor
+  minibatch_dim: int, total number of columns of y to sample
+  dim: int, problem dimension
   """
-  if n_min == n_max:
-    return [n_min]
-  idx_start = np.random.randint(n_min,n_max)
-  cols = list(range(idx_start,min([idx_start+n_cols,n_max+1])))
-  if idx_start+n_cols >n_max+1:
-    cols += list(range(n_min,n_min+(idx_start+n_cols)%(n_max+1)))
-  return cols
+  # randomly select columns of y to train on
+  idx_y   = random.sample(range(1,dim+1),minibatch_dim-1) # ensures unique entries
+  idx_y  += [0] # append 0 to the list for function values
+  idx_y.sort()
+  y_batch = y_batch[:,idx_y]
+
+  # dont pass a direction if we load function values
+  E_canonical = torch.eye(dim)
+  derivative_directions = E_canonical[idx_y[1:]]
+
+  return y_batch,derivative_directions
+
 
 
 def train_gp(train_x,train_y,num_inducing=128,
-  num_directions=1,batch_size=1024,batch_n_partials =1,num_epochs=1):
+  num_directions=1,minibatch_size=1,minibatch_dim =1,num_epochs=1):
   
   dim = train_x.size(-1)
 
-  # Multitask GP model
-  model = MultitaskGPModel(num_inducing,num_directions,dim)
-  # gaussian likelihood 
-  likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_directions+1)
+  # initialize model
+  model = GPModel(num_inducing,num_directions,dim)
+  likelihood = gpytorch.likelihoods.GaussianLikelihood()
 
+  # training mode
   model.train()
   likelihood.train()
 
@@ -90,34 +130,35 @@ def train_gp(train_x,train_y,num_inducing=128,
       {'params': likelihood.parameters()},
   ], lr=0.1)
 
-  # loss; 
+  # TODO:
+  # determine if num_data=train_y.size(0) or if we should make
+  # the number of data equal to the train_y.size(0)*train_y.size(1)???
   mll = gpytorch.mlls.VariationalELBO(likelihood, model, num_data=train_y.size(0))
 
   # set up the data loader
   train_dataset = TensorDataset(train_x, train_y)
-  train_loader  = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+  train_loader  = DataLoader(train_dataset, batch_size=minibatch_size, shuffle=True)
 
+  # train
   epochs_iter = tqdm.tqdm(range(num_epochs), desc="Epoch")
   for i in epochs_iter:
-    # Within each iteration, we will go over each minibatch of data
+
+    # iterator for minibatches
     minibatch_iter = tqdm.tqdm(train_loader, desc="Minibatch", leave=False)
+
+    # loop through minibatches
     for x_batch, y_batch in minibatch_iter:
 
-      # randomly select continguous columns of partial derivs
-      # idx_deriv_contig = sample_contiguous_columns(dim,batch_n_partials,n_min=1)
-      # # prepend zero so we get function values as well
-      # idx_contig = [0] + idx_deriv_contig
-      # y_batch = y_batch[:,idx_contig]
-
-      optimizer.zero_grad()
+      # select random columns of y_batch to train on
+      y_batch,derivative_directions = select_cols_of_y(y_batch,minibatch_dim,dim)
 
       # TODO:
-      # - pass the right output to the mll so that expected_log_prob is computed
-      #   correctly. This might mean computing a different mean and kernel matrix
-      #   than what model returns
-      output = model(train_x)
-      # evaluate loss
-      loss = -mll(output, train_y)
+      # pass in the canonical directions for the selected
+      # derivatives with idx_y... i think pass in to model
+      # when computing output
+      optimizer.zero_grad()
+      output = model(train_x,derivative_directions)
+      loss   = -mll(output, train_y)
       epochs_iter.set_postfix(loss=loss.item())
       loss.backward()
       optimizer.step()
@@ -134,60 +175,24 @@ if __name__ == "__main__":
   n   = 100
   dim = 2
   train_x = torch.rand(n,dim)
-  # train_x = torch.linspace(0, 2*math.pi, n).reshape(n,dim)
-
-  # [f(x) df/dx df/dy]
-  # train_y = torch.stack([torch.sin(train_x[:,0] + train_x[:,1]),
-  #     torch.cos(train_x[:,0] + train_x[:,1]),
-  #     torch.cos(train_x[:,0] + train_x[:,1])
-  # ], -1)
-  train_y = torch.stack([torch.sin(train_x[:,0]),
-      torch.cos(train_x[:,0]),
-  ], -1)
+  # f(x) = sin(x+y), df/dx = cos(x+y), df/dy = cos(x+y)
+  train_y = torch.stack([torch.sin(train_x[:,0]+train_x[:,1]),
+    torch.cos(train_x[:,0]+train_x[:,1]),torch.cos(train_x[:,0]+train_x[:,1])], -1)
   
-  num_inducing = 20
-  num_directions = dim
-  num_epochs = 50
 
   # train
   model,likelihood = train_gp(
                         train_x,
                         train_y,
-                        num_inducing=num_inducing,
-                        num_directions=num_directions,
-                        batch_size = 1024,
-                        batch_n_partials =dim,
-                        num_epochs = num_epochs
+                        num_inducing=20,
+                        num_directions=dim,
+                        minibatch_size = 7,
+                        minibatch_dim = dim,
+                        num_epochs = 50
                         )
 
   # Set into eval mode
   model.eval()
   likelihood.eval()
 
-  # Make predictions
-  with torch.no_grad(), gpytorch.settings.fast_pred_var():
-      n_test = 33
-      # test_x = torch.rand(n_test,dim)
-      test_x = torch.linspace(0, 2*math.pi, n_test).reshape(n_test,dim)
-      predictions = likelihood(model(test_x))
-      mean = predictions.mean
-      lower, upper = predictions.confidence_region()
-  print(mean)
-  plt.plot(train_x.detach().numpy().flatten(), train_y[:, 0].detach().numpy().flatten(), 'k*')
-  plt.plot(test_x.detach().numpy().flatten(), mean[:, 0].detach().numpy().flatten(), 'b')
-
-  # Initialize plots
-  # fig, axs = plt.subplots(1, num_tasks, figsize=(4 * num_tasks, 3))
-  # for task, ax in enumerate(axs):
-  #     # Plot training data as black stars
-  #     ax.plot(train_x.detach().numpy().flatten(), train_y[:, task].detach().numpy().flatten(), 'k*')
-  #     # Predictive mean as blue line
-  #     ax.plot(test_x.detach().numpy().flatten(), mean[:, task].detach().numpy().flatten(), 'b')
-  #     # Shade in confidence
-  #     # ax.fill_between(test_x.numpy(), lower[:, task].numpy(), upper[:, task].numpy(), alpha=0.5)
-  #     ax.set_ylim([-3, 3])
-  #     # ax.legend(['Observed Data', 'Mean', 'Confidence'])
-  #     ax.set_title(f'Task {task + 1}')
-  # fig.tight_layout()
-  plt.show()
   
