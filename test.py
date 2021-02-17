c4503586ae9c94836b03a8eca96c287f4e2bc83b
Misha Padidar <map454@cornell.edu>
2021-02-16 22:29:40 -0500

    started transition to everything being giant vector form. Also trying to make things work with the variational strat.... still needs work

--- test.py
+++ test.py
@@ -10,22 +10,22 @@ from RBFKernelDirectionalGrad import RBFKernelDirectionalGrad
 from DirectionalGradVariationalStrategy import DirectionalGradVariationalStrategy
 
 """Notes from Jake
-- we will pass in mini-batches of data stochastically with a fixed size of observations,
+[x] we will pass in mini-batches of data stochastically with a fixed size of observations,
   i.e. function values plus fixed number of derivatives
-- when passing in training data we should pass in X,[fX,df/dW],W where W is the set
+[] when passing in training data we should pass in X,[fX,df/dW],W where W is the set
   of canonical directions that the mini-batch of derivatives df/dW are taken in.
-- upon taking block of observations we should reshape observations as a giant vector
+[x] upon taking block of observations we should reshape observations as a giant vector
   so that [fX,df/dW] is a giant vector. This means we should repeat the X values so that 
   the vector of observations is the same number of entries as X. 
-- The kernel K_{XZ} is computes the kernel at the points X and Z in the directions U and V. 
+[] The kernel K_{XZ} computes the kernel at the points X and Z in the directions U and V. 
   When X is training points the directions are W, the canonical directions that match up
   with the derivative directions of the mini-batch. The directions V are the inducing directions. 
   We must rewrite the kernel to require two sets of points and two sets of directions.
-- q(f) should be a vector of same size as the number of observations. We should edit the
+[] q(f) should be a vector of same size as the number of observations. We should edit the
   base variational strategy so that we can pass in kernel directions. I beleive that we can
   do this through kwargs.
-- make sure to only return the diagonal of q(f)
-- One big takeaway is that their is no multitasking at all!"""
+[] make sure to only return the diagonal of q(f)
+[x] One big takeaway is that their is no multitasking at all!"""
 
 """Future Upgrades
 - don't include function values in every training iteration... be truly stochastic.
@@ -45,40 +45,16 @@ class GPModel(gpytorch.models.ApproximateGP):
         variational_distribution = gpytorch.variational.DeltaVariationalDistribution(
             num_inducing + num_directional_derivs)
         # variational strategy q(f)
-        variational_strategy = DirectionalGradVariationalStrategy(gpytorch.variational.VariationalStrategy(
-                self, inducing_points, variational_distribution, learn_inducing_locations=True),
-        num_directions
-        )
-        super().__init__(variational_strategy)
+        variational_strategy = DirectionalGradVariationalStrategy(self, 
+          inducing_points,inducing_directions, variational_distribution, learn_inducing_locations=True)
+        super(GPModel, self).__init__(variational_strategy)
 
-        # TODO: 
-        # - mark the mean and covariance as batch (see example)
-        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([num_directions+1]))
-        self.covar_module = gpytorch.kernels.ScaleKernel(RBFKernelDirectionalGrad(batch_shape=torch.Size([num_directions+1])),
-          batch_shape=torch.Size([num_directions+1]))
-        # set the number of directions
-        self.covar_module.base_kernel.set_n_dir(num_directions)
-
-        # this is the LMC covar... just here for testing
-        # self.covar_module = gpytorch.kernels.ScaleKernel(
-        #     gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_directions+1])),
-        #     batch_shape=torch.Size([num_directions+1])
-        # )
-
-        # register the directions
-        self.register_parameter(name="inducing_directions", parameter=torch.nn.Parameter(inducing_directions))
-        # print(variational_strategy(torch.rand(10, dim)).shape)
-        # quit()
+        # set the mean and covariance
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(RBFKernelDirectionalGrad())
 
 
-
-    def forward(self, x, derivative_directions):
-
-        # pass in params
-        params = {}
-        params['V']  =  self.inducing_directions.data
-        params['num_inducing'] = self.num_inducing
-
+    def forward(self, x, **params):
         mean_x  = self.mean_module(x)
         covar_x = self.covar_module(x, **params)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
@@ -93,18 +69,18 @@ def select_cols_of_y(y_batch,minibatch_dim,dim):
 
   input
   y_batch: 2D-torch tensor
-  minibatch_dim: int, total number of columns of y to sample
+  minibatch_dim: int, total number of derivative columns of y to sample
   dim: int, problem dimension
   """
   # randomly select columns of y to train on
-  idx_y   = random.sample(range(1,dim+1),minibatch_dim-1) # ensures unique entries
+  idx_y   = random.sample(range(1,dim+1),minibatch_dim) # ensures unique entries
   idx_y  += [0] # append 0 to the list for function values
   idx_y.sort()
   y_batch = y_batch[:,idx_y]
 
   # dont pass a direction if we load function values
   E_canonical = torch.eye(dim)
-  derivative_directions = E_canonical[idx_y[1:]]
+  derivative_directions = E_canonical[np.array(idx_y[1:])-1]
 
   return y_batch,derivative_directions
 
@@ -123,17 +99,13 @@ def train_gp(train_x,train_y,num_inducing=128,
   model.train()
   likelihood.train()
 
-  # TODO:
-  # - find out if the hyperparameters are passed through the kernel to here
   optimizer = torch.optim.Adam([
       {'params': model.parameters()},
       {'params': likelihood.parameters()},
   ], lr=0.1)
 
-  # TODO:
-  # determine if num_data=train_y.size(0) or if we should make
-  # the number of data equal to the train_y.size(0)*train_y.size(1)???
-  mll = gpytorch.mlls.VariationalELBO(likelihood, model, num_data=train_y.size(0))
+  num_data = train_y.size(0)*train_y.size(1)
+  mll = gpytorch.mlls.VariationalELBO(likelihood, model, num_data=num_data)
 
   # set up the data loader
   train_dataset = TensorDataset(train_x, train_y)
@@ -151,14 +123,20 @@ def train_gp(train_x,train_y,num_inducing=128,
 
       # select random columns of y_batch to train on
       y_batch,derivative_directions = select_cols_of_y(y_batch,minibatch_dim,dim)
+      kwargs = {}
+      kwargs['derivative_directions'] = derivative_directions
 
-      # TODO:
-      # pass in the canonical directions for the selected
-      # derivatives with idx_y... i think pass in to model
-      # when computing output
+      # flatten function and derivatives
+      # [function values, derivative entry 1, derivative entry 2,...]
+      y_batch = torch.reshape(y_batch.T,(minibatch_size*(minibatch_dim+1),1))
+      # repeat x values to match new y_batch shape
+      x_batch = x_batch.repeat(minibatch_dim+1,1)
+
       optimizer.zero_grad()
-      output = model(train_x,derivative_directions)
-      loss   = -mll(output, train_y)
+      output = model(x_batch,**kwargs)
+      print(output)
+      quit()
+      loss   = -mll(output, y_batch)
       epochs_iter.set_postfix(loss=loss.item())
       loss.backward()
       optimizer.step()
