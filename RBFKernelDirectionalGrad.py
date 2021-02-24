621775f89e2297b79a87cb9a684a0d38c6a0f740
Misha Padidar <map454@cornell.edu>
2021-02-24 09:32:21 -0500

    v1 and v2 are expected to have directions for each point in x1 and x2 respectively, rather than v1 and v2 being a set of global directions

--- RBFKernelDirectionalGrad.py
+++ RBFKernelDirectionalGrad.py
@@ -7,7 +7,14 @@ from gpytorch.kernels.rbf_kernel import RBFKernel, postprocess_rbf
 
 class RBFKernelDirectionalGrad(RBFKernel):
     r"""
-    Pass in v1 and v2 through the params.
+    Pass in v1 and v2 through the params. If v1 has n_dir1 directions per
+    point in x2 then it should be shape n1*n_dir1 x dim. The directions
+    are assumed to be stored in blocks so that the first n_dir1 directions
+    belong to x1[0] and the second n_dir1 directions belong to x1[1] etc.
+    
+    If you have a single set of global directions such as torch.eye(dim), then 
+    you can repeat those to make v1 and v2 with 
+    v1 = torch.eye(dim).repeat(n1,1)
 
     Args:
         :attr:`batch_shape` (torch.Size, optional):
@@ -39,8 +46,9 @@ class RBFKernelDirectionalGrad(RBFKernel):
 
         v1 = params['v1']
         v2 = params['v2']
-        n_dir1 = v1.shape[-2]
-        n_dir2 = v2.shape[-2]
+        # number of directions per point
+        n_dir1 = int(v1.shape[-2]/n1)
+        n_dir2 = int(v2.shape[-2]/n2)
         assert n_dir1 == n_dir2, "v1 and v2 must contain same number of directions"
 
         self.set_num_directions(n_dir1)
@@ -56,42 +64,40 @@ class RBFKernelDirectionalGrad(RBFKernel):
             x1_ = x1.div(self.lengthscale)
             x2_ = x2.div(self.lengthscale)
 
-            # project inputs onto directions
-            x1_v1 = x1_ @ v1.T
-            x1_v2 = x1_ @ v2.T
-            x2_v1 = x2_ @ v1.T
-            x2_v2 = x2_ @ v2.T
-
-            # Form all possible rank-1 products for the gradient and Hessian blocks
-            # outer = x1_.view(*batch_shape, n1, 1, d) - x2_.view(*batch_shape, 1, n2, d)
-            # outer = outer / self.lengthscale.unsqueeze(-2)
-            # outer = torch.transpose(outer, -1, -2).contiguous()
-
             # 1) Kernel block
             diff = self.covar_dist(x1_, x2_, square_dist=True, dist_postprocess_func=postprocess_rbf, **params)
             K_11 = diff
             K[..., :n1, :n2] = K_11
 
+
             # 2) First gradient block
-            outer = x1_v2.view(*batch_shape, n1, 1, n_dir2) - x2_v2.view(*batch_shape, 1, n2, n_dir2)
+            # v2 = v2.repeat(n2,1)
+            x2_v2 = x2_.reshape(n2,1,dim).bmm(torch.transpose(v2.reshape(n2,n_dir2,dim),-2,-1))
+            x1_v2 = x1_ @ v2.T
+            outer  = x1_v2 - x2_v2.flatten()
+            outer  = outer.reshape(n1,n2,n_dir2)
+            # outer = x1_v2.view(*batch_shape, n1, 1, n_dir2) - x2_v2.view(*batch_shape, 1, n2, n_dir2)
             outer = outer / self.lengthscale.unsqueeze(-2)
             outer = torch.transpose(outer, -1, -2).contiguous()
             outer1 = outer.view(*batch_shape, n1, n2 * n_dir2)
             K[..., :n1, n2:] = outer1 * K_11.repeat([*([1] * (n_batch_dims + 1)), n_dir2]) 
-            # outer1 = outer.view(*batch_shape, n1, n2 * d)
-            # K[..., :n1, n2:] = outer1 * K_11.repeat([*([1] * (n_batch_dims + 1)), d]) 
+
 
             # 3) Second gradient block
-            outer = x1_v1.view(*batch_shape, n1, 1, n_dir1) - x2_v1.view(*batch_shape, 1, n2, n_dir1)
+            # v11 = v1.repeat(n1,1)
+            x2_v1 = x2_ @ v1.T
+            x1_v1  = x1_.reshape(n1,1,dim).bmm(torch.transpose(v1.reshape(n1,n_dir1,dim),-2,-1))
+            outer  = x1_v1.flatten() - x2_v1
+            outer  = outer.reshape(n2,n1,n_dir1)
+            outer  = torch.transpose(outer,0,-2)
+            # outer = x1_v1.view(*batch_shape, n1, 1, n_dir1) - x2_v1.view(*batch_shape, 1, n2, n_dir1)
+            # print(outer)
             outer = outer / self.lengthscale.unsqueeze(-2)
             outer = torch.transpose(outer, -1, -2).contiguous()
             outer2 = outer.view(*batch_shape, n1*n_dir1, n2)
             outer2 = outer.transpose(-1, -3).reshape(*batch_shape, n2, n1 * n_dir1)
             outer2 = outer2.transpose(-1, -2)
             K[..., n1:, :n2] = -outer2 * K_11.repeat([*([1] * n_batch_dims), n_dir1, 1])
-            # outer2 = outer.transpose(-1, -3).reshape(*batch_shape, n2, n1 * d)
-            # outer2 = outer2.transpose(-1, -2)
-            # KK = -outer2 * K_11.repeat([*([1] * n_batch_dims), d, 1])
 
             # 4) Hessian block
             outer3 = outer1.repeat([*([1] * n_batch_dims), n_dir1, 1]) * outer2.repeat([*([1] * (n_batch_dims + 1)), n_dir2])
@@ -135,23 +141,26 @@ class RBFKernelDirectionalGrad(RBFKernel):
 
 if __name__ == '__main__':
   
+  torch.manual_seed(0)
   # generate training data
-  n1   = 100
-  n2   = 100
+  n1   = 5
+  n2   = 3
   dim = 4
   train_x = torch.rand(n1,dim)
   train_x2 = torch.randn(n2,dim)
   # set directions
-  n_directions = 4
+  n_directions = 3
   v1 = torch.eye(dim)[:n_directions]
+  v1 = v1.repeat(n1,1)
   v2 = torch.eye(dim)[:n_directions]
+  v2 = v2.repeat(n2,1)
   k = RBFKernelDirectionalGrad()
   params = {'v1':v1,'v2':v2}
-  K = k(train_x,train_x, **params)
+  K = k(train_x,train_x2, **params)
   print(K.detach().numpy().shape)
 
   # verify against RBFKernelGrad
   # from gpytorch.kernels import RBFKernelGrad
   # kk = RBFKernelGrad()
-  # KK = kk(train_x,train_x)
+  # KK = kk(train_x,train_x2)
   # print(KK.detach().numpy() - K.detach().numpy())
