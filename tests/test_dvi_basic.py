c439b1b4a01d2f6965f4cd71d99f477ec3c9dd84
Misha Padidar <map454@cornell.edu>
2021-03-15 13:33:33 -0400

    included model saving, prediction with minibatches

--- tests/test_dvi_basic.py
+++ tests/test_dvi_basic.py
@@ -6,7 +6,11 @@ import tqdm
 import random
 from matplotlib import pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
-from test import train_gp
+import sys
+sys.path.append("../")
+sys.path.append("../utils")
+from directional_vi import train_gp
+from metrics import MSE
 
 # torch.random.manual_seed(0)
 # generate training data
@@ -17,19 +21,19 @@ train_x = torch.rand(n,dim)
 train_y = torch.stack([torch.sin(2*np.pi*(train_x[:,0]**2+train_x[:,1]**2)),
   4*np.pi*train_x[:,0]*torch.cos(2*np.pi*(train_x[:,0]**2+train_x[:,1]**2)),
   4*np.pi*train_x[:,1]*torch.cos(2*np.pi*(train_x[:,0]**2+train_x[:,1]**2))], -1)
-# train_y = torch.stack([torch.sin(2*np.pi*train_x[:,0]),2*np.pi*torch.cos(2*np.pi*train_x[:,0]),0.0*train_x[:,1]], -1)
+# testing data
+n_test = 1000
+test_x = torch.rand(n_test,dim)
+test_y = torch.stack([torch.sin(2*np.pi*( test_x[:,0]**2+test_x[:,1]**2)),
+  4*np.pi*test_x[:,0]*torch.cos(2*np.pi*(test_x[:,0]**2+test_x[:,1]**2)),
+  4*np.pi*test_x[:,1]*torch.cos(2*np.pi*(test_x[:,0]**2+test_x[:,1]**2))], -1)
+test_dataset = TensorDataset(test_x, test_y)
+test_loader = DataLoader(test_dataset, batch_size=n_test, shuffle=False)
 
 
-# generate training data
-# n   = 100
-# dim = 1
-# train_x = torch.linspace(0,2*np.pi,n).reshape(n,dim)
-# train_y = torch.stack([torch.sin(train_x[:,0]),
-#   torch.cos(train_x[:,0])], -1)
-# train_y = torch.stack([torch.sin(train_x[:,0])+torch.exp(-3*train_x[:,0]),
-#   torch.cos(train_x[:,0]) -3*torch.exp(-3*train_x[:,0])], -1)
-
+# number of inducing directions (per inducing point) to learn
 num_directions = dim
+
 # train
 model,likelihood = train_gp(
                       train_x,
@@ -38,53 +42,57 @@ model,likelihood = train_gp(
                       num_directions=num_directions,
                       minibatch_size = 100,
                       minibatch_dim = num_directions,
-                      num_epochs =10
+                      num_epochs =1000
                       )
 
 # Set into eval mode
 model.eval()
 likelihood.eval()
-for param in model.parameters():
-  if param.requires_grad:
-    print(param.data)
 
-# predict
+# print out the trained parameters
+#for param in model.parameters():
+#  if param.requires_grad:
+#    print(param.data)
+
+# save the model
+torch.save(model.state_dict(), "../data/test_dvi_basic.model")
+
+
+# predict in batches
 kwargs = {}
 derivative_directions = torch.eye(dim)[:num_directions]
-derivative_directions = derivative_directions.repeat(n,1)
+derivative_directions = derivative_directions.repeat(n_test,1)
 kwargs['derivative_directions'] = derivative_directions
-preds   = model(train_x, **kwargs).mean.cpu()
+#means   = model(test_x, **kwargs).mean.cpu()
+means = torch.tensor([0.])
+with torch.no_grad():
+    for x_batch, y_batch in test_loader:
+        preds = model(x_batch,**kwargs)
+        means = torch.cat([means, preds.mean.cpu()])
+means = means[1:]
 
-# import matplotlib.pyplot as plt
-# pred_f  = preds[::dim+1]
-# pred_df = preds[1::dim+1]
-# plt.plot(train_x.flatten(),train_y.flatten()[::dim+1],'r-',linewidth=2,label='true f(x)')
-# plt.plot(train_x.flatten(),pred_f.detach().numpy(),'b-',linewidth=2,label='variational f(x)')
-# plt.plot(train_x.flatten(),train_y.flatten()[1::dim+1],'r--',linewidth=2,label='true df/dx')
-# plt.plot(train_x.flatten(),pred_df.detach().numpy(),'b--',linewidth=2,label='variational df/dx')
-# plt.legend()
-# plt.tight_layout()
-# plt.title("Variational GP Predictions with Learned Derivatives")
-# plt.show()
+# compute MSE
+test_mse = MSE(test_y[:,0],means[::num_directions+1])
+print(f"\nTesting MSE: {test_mse}")
 
-
+# plot it
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 fig = plt.figure(figsize=(12,6))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(train_x[:,0],train_x[:,1],train_y[:,0], color='k')
-ax.scatter(train_x[:,0],train_x[:,1],preds.detach().numpy()[::num_directions+1], color='b')
+ax.scatter(test_x[:,0],test_x[:,1],test_y[:,0], color='k')
+ax.scatter(test_x[:,0],test_x[:,1],means[::num_directions+1], color='b')
 plt.title("f(x,y) variational fit; actual curve is black, variational is blue")
 plt.show()
 fig = plt.figure(figsize=(12,6))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(train_x[:,0],train_x[:,1],train_y[:,1], color='k')
-ax.scatter(train_x[:,0],train_x[:,1],preds.detach().numpy()[1::num_directions+1], color='b')
+ax.scatter(test_x[:,0],test_x[:,1],test_y[:,1], color='k')
+ax.scatter(test_x[:,0],test_x[:,1],means[1::num_directions+1], color='b')
 plt.title("df/dx variational fit; actual curve is black, variational is blue")
 plt.show()
 fig = plt.figure(figsize=(12,6))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(train_x[:,0],train_x[:,1],train_y[:,2], color='k')
-ax.scatter(train_x[:,0],train_x[:,1],preds.detach().numpy()[2::dim+1], color='b')
+ax.scatter(test_x[:,0],test_x[:,1],test_y[:,2], color='k')
+ax.scatter(test_x[:,0],test_x[:,1],means[2::num_directions+1], color='b')
 plt.title("df/dy variational fit; actual curve is black, variational is blue")
 plt.show()
