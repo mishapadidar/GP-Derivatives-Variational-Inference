d0b6e1aed8b4d4385ac68bf65861a0e50ffe2892
Xinran Zhu <xz584@cornell.edu>
2021-03-27 22:02:19 -0400

    add RMSE

--- directionalvi/utils/metrics.py
+++ directionalvi/utils/metrics.py
@@ -11,3 +11,10 @@ def MAE(Y,Z):
   Z: torch tensor, predicted function values 
   """
   return ((Y-Z).abs()).mean()
+
+def RMSE(Y, Z):
+  """Compute the MSE.
+  Y: torch tensor, function values
+  Z: torch tensor, predicted function values 
+  """
+  return ((Y-Z)**2).mean().sqrt()
