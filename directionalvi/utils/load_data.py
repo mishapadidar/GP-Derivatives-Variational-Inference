b00723b49680b3c338c658cb48db5e1d8e5927c6
Xinran Zhu <xz584@cornell.edu>
2021-03-27 21:17:28 -0400

    add synthetic data loader

---
+++ directionalvi/utils/load_data.py
@@ -0,0 +1,36 @@
+from synthetic_functions import *
+from rescale import *
+
+
+def load_synthetic_data(test_fun, n, **kwargs):
+    '''
+    load synthetic data 
+    Input: 
+        test_fun: a modified Botorch test functions
+        n: number of datapoints
+    Output: 
+        x: torch tensor, random data from unit cube
+        y: torch tensor, normalized and rescaled labels (w/ or w/o derivatives)
+    '''
+    torch.random.manual_seed(kwargs["seed"])
+    x_unit = torch.rand(n,dim)
+    # evaluate in the true range
+    lb, ub = testfun.get_bounds()
+    x = from_unit_cube(x_unit, lb, ub)
+    if kwargs["derivative"]:
+        y = testfun.evaluate_true_with_deriv(train_x)
+    else:
+        y = testfun.evaluate_true(train_x)
+    # normalize y values
+    normalize(y, **kwargs)
+    if kwargs["derivative"]:
+        # mapping derivative values to unit cube
+        f = y[..., 0].reshape(len(y),1)
+        g = y[..., 1:].reshape(len(y),-1)
+        g *= (ub - lb)
+        y = torch.cat([f, g], 1)
+    return x_unit, y
+
+
+
+# def load_real_data(src, n, **kwargs):
