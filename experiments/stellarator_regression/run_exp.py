ab196d37f2dce33752947804f43030efcb7f5c18
Misha Padidar <map454@cornell.edu>
2021-03-29 21:33:29 -0400

    new parameter and some new flags to drop files or not

--- experiments/stellarator_regression/run_exp.py
+++ experiments/stellarator_regression/run_exp.py
@@ -3,22 +3,27 @@ import pickle
 import subprocess
 from datetime import datetime
 
+# flags
+write_sbatch =True
+submit       =True
+
 # write a pickle file with the run info
 run_params_dir = "./param_files/"
 if os.path.exists(run_params_dir) is False:
   os.mkdir(run_params_dir)
 run_params = {}
 run_params['num_inducing']                 = 1024
-run_params['num_directions']               = 2
-run_params['minibatch_size']               = 1024
-run_params['num_epochs']                   = 1
+run_params['num_directions']               = 5
+run_params['minibatch_size']               = 512
+run_params['num_epochs']                   = 300
 run_params['tqdm']                         = False
 run_params['inducing_data_initialization'] = False
 run_params['use_ngd']                      = False
 run_params['use_ciq']                      = False
 run_params['learning_rate_hypers']         = 1e-3
 run_params['learning_rate_ngd']            = 1e-3
-run_params['gamma']                        = 10
+run_params['gamma']                        = 0
+run_params['data_file'] = "../../data/focus_w7x_dataset.csv"
 # seed and date
 now     = datetime.now()
 seed    = int("%d%.2d%.2d%.2d%.2d"%(now.month,now.day,now.hour,now.minute,now.second))
@@ -32,34 +37,39 @@ base_name = f"stell_regress_ni_{run_params['num_inducing']}_nd_{run_params['num_
 run_params['base_name']  = base_name
 param_filename = run_params_dir + "params_" +base_name + ".pickle"
 pickle.dump(run_params,open(param_filename,'wb'))
+print(f"Dumped param file: {param_filename}")
 
-# write a slurm batch script
-slurm_dir  = "./slurm_scripts/"
-if os.path.exists(slurm_dir) is False:
-  os.mkdir(slurm_dir)
-slurm_name = slurm_dir + base_name + ".sub"
-#slurm_name = base_name + ".sub"
-f = open(slurm_name,"w")
-f.write(f"#!/bin/bash\n")
-f.write(f"#SBATCH -J  stell\n")
-f.write(f"#SBATCH -o ./slurm_output/job_{barcode}.out\n")
-f.write(f"#SBATCH -e ./slurm_output/job_{barcode}.err\n")
-f.write(f"#SBATCH --get-user-env\n")
-f.write(f"#SBATCH -N 1\n")
-f.write(f"#SBATCH -n 1\n")
-f.write(f"#SBATCH --mem=15000\n")
-f.write(f"#SBATCH -t 168:00:00\n")
-f.write(f"#SBATCH --partition=default_gpu\n")
-f.write(f"#SBATCH --gres=gpu:1\n")
-f.write(f"python3 stellarator_regression.py {param_filename}\n")
+if write_sbatch:
+  # write a slurm batch script
+  slurm_dir  = "./slurm_scripts/"
+  if os.path.exists(slurm_dir) is False:
+    os.mkdir(slurm_dir)
+  slurm_name = slurm_dir + base_name + ".sub"
+  #slurm_name = base_name + ".sub"
+  f = open(slurm_name,"w")
+  f.write(f"#!/bin/bash\n")
+  f.write(f"#SBATCH -J  stell\n")
+  f.write(f"#SBATCH -o ./slurm_output/job_%j.out\n")
+  f.write(f"#SBATCH -e ./slurm_output/job_%j.err\n")
+  f.write(f"#SBATCH --get-user-env\n")
+  f.write(f"#SBATCH -N 1\n")
+  f.write(f"#SBATCH -n 1\n")
+  f.write(f"#SBATCH --mem=15000\n")
+  f.write(f"#SBATCH -t 168:00:00\n")
+  f.write(f"#SBATCH --partition=default_gpu\n")
+  f.write(f"#SBATCH --gres=gpu:1\n")
+  f.write(f"python3 stellarator_regression.py {param_filename}\n")
+  print(f"Dumped slurm file: {slurm_name}")
+  
+  # write the shell submission script
+  submit_name = slurm_dir + 'slurm_submit.sh'
+  f = open(submit_name,"w")
+  f.write(f"#!/bin/bash\n")
+  f.write(f"sbatch {slurm_name}")
+  f.close()
+  print(f"Dumped bash script: {submit_name}")
 
-# write the shell submission script
-submit_name = slurm_dir + 'slurm_submit.sh'
-f = open(submit_name,"w")
-f.write(f"#!/bin/bash\n")
-f.write(f"sbatch {slurm_name}")
-f.close()
-
-# submit the script
-bash_command = f"sbatch {slurm_name}"
-subprocess.run(bash_command.split(" "))
+if submit:
+  # submit the script
+  bash_command = f"sbatch {slurm_name}"
+  subprocess.run(bash_command.split(" "))
