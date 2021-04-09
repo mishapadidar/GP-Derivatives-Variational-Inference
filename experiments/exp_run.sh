f7eab2aa0520a929817c60c355c5c06fbaa4c209
Xinran Zhu <xz584@cornell.edu>
2021-04-09 12:11:46 -0400

    add save reslults feature

--- experiments/exp_run.sh
+++ experiments/exp_run.sh
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#dataset="synthetic-Branin" # synthetic/real - dataset name 
-dataset="synthetic-StyblinskiTang"
-
-# exp setups
-n_train=1000
-n_test=500
-num_inducing=150
-num_directions=2
-num_epochs=2
-batch_size=200
-=======
 dataset="synthetic-Branin" # synthetic/real - dataset name 
 # dataset="real-helens"
 
@@ -20,14 +8,15 @@ num_inducing=500
 num_directions=2
 num_epochs=1000
 batch_size=512
->>>>>>> 69ca24e9d013bb17120414901c53ad0a13b69c19
 lr=0.01
 lr_ngd=0.1
 num_contour_quad=15
-watch_model=True
+watch_model=False
 exp_name="TEST"
 seed=0
 lr_sched="step_lr"
+save_results=False
+
 # compare different methods, comment out the chunk if not comparing with this method
 # find runlogs in logs folder
 
@@ -37,16 +26,15 @@ variational_distribution="standard"
 sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
                   ${n_train} ${n_test} ${num_inducing}\
                   ${num_directions} ${num_epochs} ${batch_size} ${model}\
-                  ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched}
+                  ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched} ${save_results}
 
-<<<<<<< HEAD
 # model="DSVGP"
 # variational_strategy="standard"
 # variational_distribution="NGD"
 # sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
 #                   ${n_train} ${n_test} ${num_inducing}\
 #                   ${num_directions} ${num_epochs} ${batch_size} ${model}\
-#                   ${lr} ${lr_ngd} ${num_contour_quad}
+#                   ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched} ${save_results}
 
 # model="DSVGP"
 # variational_strategy="CIQ"
@@ -54,41 +42,14 @@ sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}
 # sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
 #                   ${n_train} ${n_test} ${num_inducing}\
 #                   ${num_directions} ${num_epochs} ${batch_size} ${model}\
-#                   ${lr} ${lr_ngd} ${num_contour_quad}
+#                   ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched} ${save_results}
 
 # for traditional SVGP, 
 # variational_strategy and variational_distribution don't matter, but need to pass in them.
 # model="SVGP"
 # variational_strategy="standard"
-# variational_distribution="NGD"
+# variational_distribution="standard"
 # sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
 #                   ${n_train} ${n_test} ${num_inducing}\
 #                   ${num_directions} ${num_epochs} ${batch_size} ${model}\
-#                   ${lr} ${lr_ngd} ${num_contour_quad}
-=======
-model="DSVGP"
-variational_strategy="standard"
-variational_distribution="NGD"
-sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
-                  ${n_train} ${n_test} ${num_inducing}\
-                  ${num_directions} ${num_epochs} ${batch_size} ${model}\
-                  ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched}
-
-model="DSVGP"
-variational_strategy="CIQ"
-variational_distribution="NGD"
-sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
-                  ${n_train} ${n_test} ${num_inducing}\
-                  ${num_directions} ${num_epochs} ${batch_size} ${model}\
-                  ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched}
-
-# for traditional SVGP, 
-# variational_strategy and variational_distribution don't matter, but need to pass in them.
-model="SVGP"
-variational_strategy="standard"
-variational_distribution="standard"
-sh ./exp_setup.sh ${dataset} ${variational_strategy} ${variational_distribution}\
-                  ${n_train} ${n_test} ${num_inducing}\
-                  ${num_directions} ${num_epochs} ${batch_size} ${model}\
-                  ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched}
->>>>>>> 69ca24e9d013bb17120414901c53ad0a13b69c19
+#                   ${lr} ${lr_ngd} ${num_contour_quad} ${watch_model} ${exp_name} ${seed} ${lr_sched} ${save_results}
