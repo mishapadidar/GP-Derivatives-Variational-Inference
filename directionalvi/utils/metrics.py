17091ad4adcd801e8ce992e14b9a6224bf920b1d
Xinran Zhu <xz584@cornell.edu>
2021-04-12 21:56:41 -0400

    add SMAE metric

--- directionalvi/utils/metrics.py
+++ directionalvi/utils/metrics.py
@@ -18,3 +18,10 @@ def RMSE(Y, Z):
   Z: torch tensor, predicted function values 
   """
   return ((Y-Z)**2).mean().sqrt()
+
+def SMAE(Y, Z):
+  """Compute the MSE.
+  Y: torch tensor, function values
+  Z: torch tensor, predicted function values 
+  """
+  return ((Y-Z).abs()).mean() / Y.abs().mean()
